d5102bba6b3ab4137e69effe353813fb83c8005e
Tea Rose <tonidebelen2020@u.northwestern.edu>
2020-12-02 08:07:27 -0600

    added dev dependencies

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -7,20 +7,15 @@ mongoose.connect(
 );
 
 const bookSeed = [
-    {
-        title: "The Everything Store: Jeff Bezos and the Age of Amazon",
-        author: "Brad Stone",
-        synopsis:
-          "The definitive story of Amazon.com, one of the most successful companies in the world, and of its driven, brilliant founder, Jeff Bezos. Amazon.com started off delivering books through the mail. But its visionary founder, Jeff Bezos, wasn't content with being a bookseller. He wanted Amazon to become the everything store, offering limitless selection and seductive convenience at disruptively low prices. To do so, he developed a corporate culture of relentless ambition and secrecy that's never been cracked. Until now. Brad Stone enjoyed unprecedented access to current and former Amazon employees and Bezos family members, giving listeners the first in-depth, fly-on-the-wall account of life at Amazon. Compared to tech's other elite innovators - Jobs, Gates, Zuckerberg - Bezos is a private man. But he stands out for his restless pursuit of new markets, leading Amazon into risky new ventures like the Kindle and cloud computing, and transforming retail in the same way Henry Ford revolutionized manufacturing. The Everything Store will be the revealing, definitive biography of the company that placed one of the first and largest bets on the Internet and forever changed the way we shop and read.",
-        date: new Date(Date.now())
-      },
-      {
-        title: "1984",
-        author: "George Orwell",
-        synopsis:
-          "Winston Smith toes the Party line, rewriting history to satisfy the demands of the Ministry of Truth. With each lie he writes, Winston grows to hate the Party that seeks power for its own sake and persecutes those who dare to commit thoughtcrimes. But as he starts to think for himself, Winston can’t escape the fact that Big Brother is always watching... A startling and haunting vision of the world, 1984 is so powerful that it is completely convincing from start to finish. No one can deny the influence of this novel, its hold on the imaginations of multiple generations of readers, or the resiliency of its admonitions—a legacy that seems only to grow with the passage of time.",
-        date: new Date(Date.now())
-      },
+  {
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: "J.K. Rowling",
+    thumbnail: "https://lh3.googleusercontent.com/42ZwafOz6sg9l7gXXh_5_d4VAvx7qObVOLtVHBTBTcUbhK0ffUJUtvvdf4hC2ZLb48s=w400-h600-rw",
+    href: "https://books.google.com/books?id=HBVunAEACAAJ&dq=Harry+Potter+and+the+Sorcerer%27s+Stone&hl=&cd=2&source=gbs_api",
+    description:
+      "Harry Potter has no idea how famous he is. That's because he's being raised by his miserable aunt and uncle who are terrified Harry will learn that he's really a wizard, just as his parents were. But everything changes when Harry is summoned to attend an infamous school for wizards, and he begins to discover some clues about his illustrious birthright. From the surprising way he is greeted by a lovable giant, to the unique curriculum and colorful faculty at his unusual school, Harry finds himself drawn deep inside a mystical world he never knew existed and closer to his own noble destiny.",
+    date: new Date(Date.now())
+  },
 ];
 
 db.Book
