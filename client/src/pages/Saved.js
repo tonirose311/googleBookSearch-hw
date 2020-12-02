5fd10b1980db24849cf89bd29b52ccaea686f4a2
Tea Rose <tonidebelen2020@u.northwestern.edu>
2020-12-02 07:02:54 -0600

    corrected path mistake

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
-import { Link } from "react-router-dom";
 import {  Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
 
