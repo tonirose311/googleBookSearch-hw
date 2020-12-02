2ea1768d7cc62bd3090ec02f6924e6dcac9c0c39
Tea Rose <tonidebelen2020@u.northwestern.edu>
2020-12-02 04:26:48 -0600

    added findAll and findById methods

---
+++ controllers/booksController.js
@@ -0,0 +1,17 @@
+const db = require("../models");
+
+module.exports = {
+  findAll: function(req, res) {
+    db.Book
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Book
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+};
