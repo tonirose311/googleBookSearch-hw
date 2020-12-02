f562aaa04ef8467bfde4340cc9251ba2d61879d4
Tea Rose <tonidebelen2020@u.northwestern.edu>
2020-12-02 04:31:24 -0600

    added update and remove method

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -20,4 +20,10 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  update: function(req, res) {
+    db.Book
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
 };
